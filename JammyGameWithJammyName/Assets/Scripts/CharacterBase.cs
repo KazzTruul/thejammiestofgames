c72ce6c59d72dcfe35a622596e69d2a5998a915a
KazzTruul <kazztruul@hotmail.com>
2020-01-18 23:44:09 +0100

    more stuff

--- JammyGameWithJammyName/Assets/Scripts/CharacterBase.cs
+++ JammyGameWithJammyName/Assets/Scripts/CharacterBase.cs
@@ -29,35 +29,49 @@ public abstract class CharacterBase : MonoBehaviour
     private GameObject _leftWeaponObject;
     [SerializeField]
     private GameObject _rightWeaponObject;
+    [SerializeField]
+    private float _maxMoveSpeed;
 
     private int _currentHealth;
     private bool _invulnerable;
-    private bool _canAttack = true;
+    private bool _isAttacking;
+    private bool _isAirborne;
+
+    private BoxCollider2D _boxCollider;
 
     protected int CurrentHealth => _currentHealth;
     protected int MaxHealth => _maxHealth;
+    protected bool IsAttacking => _isAttacking;
+    protected bool IsAirborne => _isAirborne;
+
+    private void Start()
+    {
+        _boxCollider = GetComponent<BoxCollider2D>();
+    }
 
     protected virtual void Update()
     {
+        
+        _isAirborne = !_boxCollider.IsTouchingLayers(0/*FIX THIS PLZ!*/);
         _characterImage.transform.localScale = new Vector3(IsFacingLeft() ? -1f : 1f, 1f, 1f);
     }
 
-    protected virtual void Move(Vector2 direction)
+    protected virtual void Move(float velocity)
     {
-        if (direction.x == 0f && direction.y == 0f)
+        if (velocity == 0f)
         {
             _rigidbody.velocity = Vector2.zero;
             _animator.SetBool("IsMoving", false);
             return;
         }
 
-        _rigidbody.AddForce(direction * _movementSpeed * Time.deltaTime);
+        _rigidbody.AddForce(new Vector2(velocity * _movementSpeed * Time.deltaTime, 0f));
         _animator.SetBool("IsMoving", true);
     }
 
     protected virtual void Attack()
     {
-        if (!_canAttack)
+        if (_isAttacking)
         {
             return;
         }
@@ -107,7 +121,7 @@ public abstract class CharacterBase : MonoBehaviour
 
     private IEnumerator AttackCooldown()
     {
-        _canAttack = false;
+        _isAttacking = true;
 
         yield return new WaitForSeconds(_attackCooldownTime);
 
@@ -116,7 +130,7 @@ public abstract class CharacterBase : MonoBehaviour
             _leftWeaponObject.SetActive(false);
         }
 
-        _canAttack = true;
+        _isAttacking = true;
     }
 
     protected virtual void Die(CauseOfDeath causeOfDeath)
--- JammyGameWithJammyName/Assets/Scripts/PlayerCharacter.cs
+++ JammyGameWithJammyName/Assets/Scripts/PlayerCharacter.cs
@@ -11,37 +11,43 @@ public class PlayerCharacter : CharacterBase
     private float _dashCooldown;
     [SerializeField]
     private float _jumpForce;
+    [SerializeField]
+    private float _jumpTime;
 
-    private Vector2 _dashDirection;
+    private float _dashDirection;
     private float _horizontalSpeed;
     private float _verticalSpeed;
     private bool _isDashing;
+    private bool _isAirborne;
 
     protected override void Update()
     {
         _horizontalSpeed = Input.GetAxis("Horizontal");
         _verticalSpeed = Input.GetAxis("Vertical");
 
-        if (Input.GetButtonDown("Fire"))
+        if (!_isAirborne && !_isDashing && !IsAttacking)
         {
-            Attack();
-        }
+            if (Input.GetButtonDown("Fire"))
+            {
+                Attack();
+            }
 
-        if (Input.GetButtonDown("Jump"))
-        {
-            Jump();
-        }
+            if (Input.GetButtonDown("Jump"))
+            {
+                StartCoroutine(Jump());
+            }
 
-        if (Input.GetButtonDown("Dash"))
-        {
-            Dash();
-        }
+            if (Input.GetButtonDown("Dash"))
+            {
+                Dash();
+            }
 
-        Move(new Vector2(_horizontalSpeed, _verticalSpeed));
+            Move(_horizontalSpeed);
+        }
 
         base.Update();
     }
-    
+
     private void Dash()
     {
         StartCoroutine(DashTimer());
@@ -51,12 +57,12 @@ public class PlayerCharacter : CharacterBase
     {
         _isDashing = true;
 
-        _dashDirection = new Vector2(Mathf.Round(_horizontalSpeed), Mathf.Round(_verticalSpeed));
+        _dashDirection = Mathf.Round(_horizontalSpeed);
 
         float dashStartTime = Time.time;
         while (Time.time < dashStartTime + _dashTime)
         {
-            _rigidbody.AddForce(_dashDirection * _dashSpeed);
+            _rigidbody.AddForce(new Vector2(_dashDirection * _dashSpeed, 0f));
             yield return new WaitForEndOfFrame();
         }
 
@@ -69,9 +75,14 @@ public class PlayerCharacter : CharacterBase
         UIManager.Instance.SetHeroHealth(CurrentHealth, MaxHealth);
     }
 
-    private void Jump()
+    private IEnumerator Jump()
     {
-        _rigidbody.AddForce(new Vector2(0f, _jumpForce));
+        float jumpStartTime = Time.time;
+        while (Time.time < jumpStartTime + _jumpTime)
+        {
+            _rigidbody.AddForce(new Vector2(0f, _jumpForce));
+            yield return new WaitForEndOfFrame();
+        }
     }
 
     protected override void Die(CauseOfDeath causeOfDeath)
@@ -82,7 +93,7 @@ public class PlayerCharacter : CharacterBase
 
     protected override void HandleIncomingDamageEffects(int damage)
     {
-        throw new System.NotImplementedException();
+
     }
 
     protected override bool IsFacingLeft()
