b9147ab0058ec34cd1b36977c302ac23f7621ebd
@MoaLindgren <moa.lindgren@outlook.com>
2020-01-18 22:25:30 +0100

    Dialogue System, finalized, sort of

--- JammyGameWithJammyName/Assets/Scripts/DialogueManager.cs
+++ JammyGameWithJammyName/Assets/Scripts/DialogueManager.cs
@@ -1,5 +1,6 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
+using System.Linq;
 using UnityEngine;
 using TMPro;
 
@@ -13,36 +14,59 @@ public class DialogueManager : MonoBehaviour
         Funny = 3,
         Suspicious = 4,
         End = 5
-    } 
+    }
     [SerializeField] private float timer;
     [SerializeField] private TextMeshPro heroText, bossText;
+    [SerializeField] private Transform hero, boss;
     [SerializeField] private DialogueDataContainer[] initialDialogues, generalDialogues, combatDialogues, funnyDialogues, suspiciousDialogues, endDialogues;
 
-
-    private List<DialogueDataContainer[]> dialogueContainers;
-    private DialogueType dialogueType;
-    DialogueDataContainer currentDialogue;
-    GameObject currentTextObject, prevTextObject;
+    private Dictionary<DialogueType, DialogueDataContainer[]> dialogueContainers;
+    private DialogueDataContainer[] currentDialogueContainer;
+    private DialogueDataContainer currentDialogue;
+    private GameObject currentTextObject, prevTextObject;
+    private bool handlesDialogue;
 
     private void Start()
     {
-        dialogueContainers = new List<DialogueDataContainer[]>() { initialDialogues, generalDialogues, combatDialogues, funnyDialogues, suspiciousDialogues, endDialogues };
-        dialogueType = DialogueType.Initial;
+        dialogueContainers = new Dictionary<DialogueType, DialogueDataContainer[]>()
+        { { DialogueType.Initial, initialDialogues }, { DialogueType.General, generalDialogues },{ DialogueType.Combat, combatDialogues },
+          { DialogueType.Funny, funnyDialogues }, { DialogueType.Suspicious, suspiciousDialogues },{ DialogueType.End, endDialogues } };
     }
 
     private void Update()
     {
         if (Input.GetKeyDown(KeyCode.Space))
         {
-            SetDialogue();
+            SetDialogue(DialogueType.Initial);
         }
     }
 
-    private void SetDialogue()
+    private void SetDialogue(DialogueType dialogueType)
     {
-        DialogueDataContainer[] currentDialogueContainer = dialogueContainers[(int)dialogueType];
-        int rnd = Random.Range(0, currentDialogueContainer.Length - 1);
-        currentDialogue = currentDialogueContainer[rnd];
+        if (handlesDialogue)
+        {
+            return;
+        }
+
+        handlesDialogue = true;
+        DialogueDataContainer[] value;
+        if (dialogueContainers.TryGetValue(dialogueType, out value))
+        {
+            currentDialogueContainer = value;
+        }
+
+        var unusedDialogues = currentDialogueContainer.Where(cD => !cD.used).ToArray();
+
+        if (unusedDialogues.Length <= 0)
+        {
+            handlesDialogue = false;
+            ResetDialogues();
+            SetDialogue(dialogueType);
+            return;
+        }
+
+        int rnd = Random.Range(0, unusedDialogues.Length);
+        currentDialogue = unusedDialogues[rnd];
         StartCoroutine(ViewDialogue());
     }
 
@@ -50,7 +74,7 @@ public class DialogueManager : MonoBehaviour
     {
         DialogueData[] dialogue = currentDialogue.dialogueData;
 
-        for(int i = 0; i < currentDialogue.dialogueData.Length; i++)
+        for (int i = 0; i < dialogue.Length; i++)
         {
             Character character = dialogue[i].character;
             switch (character)
@@ -64,7 +88,7 @@ public class DialogueManager : MonoBehaviour
                     currentTextObject = bossText.gameObject;
                     break;
             }
-            if(prevTextObject != null)
+            if (prevTextObject != null)
             {
                 prevTextObject.SetActive(false);
             }
@@ -72,8 +96,25 @@ public class DialogueManager : MonoBehaviour
             currentTextObject.SetActive(true);
             yield return new WaitForSeconds(timer);
         }
-        prevTextObject.SetActive(false);
         yield return new WaitForSeconds(timer);
+        prevTextObject.SetActive(false);
+        currentDialogue.used = true;
+        handlesDialogue = false;
     }
-    
+
+    private void ResetDialogues()
+    {
+        foreach (var container in dialogueContainers)
+        {
+            foreach (var dialogue in container.Value)
+            {
+                dialogue.used = false;
+            }
+        }
+    }
+
+    private void OnDestroy()
+    {
+        ResetDialogues();
+    }
 }
