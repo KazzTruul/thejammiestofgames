6bfd0d2aaa394f024afda88ff83a42ef91134c35
KazzTruul <kazztruul@hotmail.com>
2020-01-18 15:42:29 +0100

    stuff

---
+++ JammyGameWithJammyName/Assets/Scripts/BossCharacter.cs
@@ -0,0 +1,17 @@
+﻿public class BossCharacter : CharacterBase
+{
+    protected override void Attack()
+    {
+        throw new System.NotImplementedException();
+    }
+
+    protected override void Die()
+    {
+        throw new System.NotImplementedException();
+    }
+
+    protected override void HandleIncomingDamageEffects(int damage)
+    {
+        throw new System.NotImplementedException();
+    }
+}
---
+++ JammyGameWithJammyName/Assets/Scripts/CharacterBase.cs
@@ -0,0 +1,57 @@
+﻿using System.Collections;
+using UnityEngine;
+
+public abstract class CharacterBase : MonoBehaviour
+{
+    [SerializeField]
+    private int _maxHealth;
+    [SerializeField]
+    protected float _movementSpeed;
+    [SerializeField]
+    protected Animator _animator;
+    [SerializeField]
+    private float _invulnerabilityTime;
+
+    private float _currentHealth;
+    private bool _invulnerable;
+
+    protected virtual void Attack()
+    {
+        _animator.SetTrigger("Attack");
+    }
+
+    private void TakeDamage(int damage)
+    {
+        HandleIncomingDamageEffects(damage);
+
+        if(_invulnerable)
+        {
+            return;
+        }
+
+        _currentHealth -= damage;
+        if (_currentHealth <= 0)
+        {
+            Die();
+            return;
+        }
+
+        _animator.SetTrigger("IncomingDamage");
+
+        StartCoroutine(Invulnerability());
+    }
+
+    protected abstract void HandleIncomingDamageEffects(int damage);
+
+    private IEnumerator Invulnerability()
+    {
+        _invulnerable = true;
+        yield return new WaitForSeconds(_invulnerabilityTime);
+        _invulnerable = false;
+    }
+
+    protected virtual void Die()
+    {
+        _animator.SetTrigger("Die");
+    }
+}
