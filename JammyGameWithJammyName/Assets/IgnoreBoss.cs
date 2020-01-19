23f98bf6ec454d11d6399af769081a3978dbed90
@MoaLindgren <moa.lindgren@outlook.com>
2020-01-19 12:28:28 +0100

    update scene

---
+++ JammyGameWithJammyName/Assets/IgnoreBoss.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class IgnoreBoss : MonoBehaviour
+{
+    [SerializeField] GameObject boss;
+    private void Update()
+    {
+        Physics2D.IgnoreCollision(gameObject.GetComponent<Collider2D>(), boss.GetComponent<Collider2D>());
+    }
+}
